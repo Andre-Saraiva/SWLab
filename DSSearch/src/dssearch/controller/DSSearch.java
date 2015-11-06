c7b7b220d03c0a9453074ceabcad8d80e539fd6b
Luiz Leme <lapaesleme@gmail.com>
2015-11-06 08:57:18 -0200

    ok

--- DSSearch/src/dssearch/controller/DSSearch.java
+++ DSSearch/src/dssearch/controller/DSSearch.java
@@ -15,12 +15,24 @@ public class DSSearch extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		response.getWriter().write("DSSearch template");
+		response.getWriter().write(write_form());
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.getWriter().write("Error: POST method not implemented.");
+	}
+	
+	private String write_form(){
+		String form="";
+		form+="<html>\n";
+		form+="<head>\n";
+		form+="<center><h1>Dataset Search</h1></center>\n";
+		form+="</head>\n";
+		form+="<body>\n";
+		form+="</body>\n";
+		form+="</html>\n";
+		return form;
 	}
 }
