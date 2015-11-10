cbcd7b67dbf986edfbbe4076898bc681dcadde3c
Luiz Leme <lapaesleme@gmail.com>
2015-11-10 12:04:33 -0200

    ok

--- DSSearch/src/dssearch/controller/NewSearch.java
+++ DSSearch/src/dssearch/controller/NewSearch.java
@@ -33,8 +33,8 @@ public class NewSearch extends HttpServlet {
 		else
 			ontVoid = getOntVoid(request.getParameter("url").toString());
 		String method = request.getParameter("method").toString();
-		int limit = 100;
-		int offset = 0;
+		int limit = 12;
+		int offset = 10;
 
 		List<Entry> result = Search.execute(ontVoid, limit, offset, method);
 		request.getSession().setAttribute("result", result);
